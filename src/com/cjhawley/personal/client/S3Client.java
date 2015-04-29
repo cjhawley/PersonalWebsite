7ba7e563541abf13d2c0f21be2cf57e8743bfcfc
Chris Hawley <cjhawley001@gmail.com>
2015-04-28 21:42:34 -0700

    Cleaning up clutter, add a small comment

--- src/com/cjhawley/personal/client/S3Client.java
+++ src/com/cjhawley/personal/client/S3Client.java
@@ -1,18 +1,16 @@
 package com.cjhawley.personal.client;
 
-
 import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
-import com.amazonaws.auth.PropertiesFileCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 
 public class S3Client {
 	private final AmazonS3 S3_CLIENT;
 	private AWSCredentialsProviderChain credentialsProviderChain;
+	
+	private static final String ROOT_BUCKET_NAME = "personal-site-cjhawley-1";
 	
 	private static S3Client INSTANCE = new S3Client();
 	
@@ -30,4 +28,7 @@ public class S3Client {
 		return S3_CLIENT;
 	}
 	
+	public static String getRootBucketName() {
+		return ROOT_BUCKET_NAME;
+	}
 }
--- src/com/cjhawley/personal/persistence/dao/impl/s3/S3PersonalEventDaoImpl.java
+++ src/com/cjhawley/personal/persistence/dao/impl/s3/S3PersonalEventDaoImpl.java
@@ -18,6 +18,7 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3Object;
 import com.cjhawley.personal.client.S3Client;
 import com.cjhawley.personal.model.PersonalEvent;
 import com.cjhawley.personal.persistence.dao.PersonalEventDao;
@@ -25,78 +26,91 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class S3PersonalEventDaoImpl implements PersonalEventDao {
 	private static final Logger LOGGER = LoggerFactory.getLogger(S3PersonalEventDaoImpl.class);
+
 	private static final Cache CACHE;
-	private static final String PERSONAL_EVENTS_KEY = "personalEvents";
+	private static final String PERSONAL_EVENTS_CACHE_KEY = "PersonalEvents";
+
+	// TODO - find a better way to keep the "object content" folder name
+	private static final String PERSONAL_EVENTS_S3_FOLDER = "personal-events";
 	
+	// only need one json mapper, since we will be converting the same type of data.
+	private final ObjectMapper MAPPER = new ObjectMapper();
+
 	static {
 		CacheManager.getInstance().addCache(S3PersonalEventDaoImpl.class.getSimpleName());
 		CACHE = CacheManager.getInstance().getCache(S3PersonalEventDaoImpl.class.getSimpleName());
 	}
-	
+
 	private static final PersonalEventDao INSTANCE = new S3PersonalEventDaoImpl();
-	
+
 	public static PersonalEventDao getInstance() {
 		return INSTANCE;
 	}
-	
+
 	private S3PersonalEventDaoImpl() {}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<PersonalEvent> getPersonalEvents() {
 		Element element;
-		if ((element = CACHE.get(PERSONAL_EVENTS_KEY)) != null) {
-			return (List<PersonalEvent>)element.getObjectValue();
+		if ((element = CACHE.get(PERSONAL_EVENTS_CACHE_KEY)) != null) {
+			return (List<PersonalEvent>) element.getObjectValue();
 		} else {
 			List<PersonalEvent> events = loadPersonalEventsFromS3();
-			CACHE.put(new Element(PERSONAL_EVENTS_KEY, events));
+			CACHE.put(new Element(PERSONAL_EVENTS_CACHE_KEY, events));
 			return events;
 		}
 	}
-	
+
 	private List<PersonalEvent> loadPersonalEventsFromS3() {
 		AmazonS3 client = S3Client.getInstance().getS3Client();
-		String bucketName = "personal-site-cjhawley-1";
-		ObjectListing listing = client.listObjects(bucketName, "personal-events");
-
-		/*
-		 * List<String> schemaKeys = listing.getObjectSummaries().stream()
-		 * .filter(s -> s.getKey().endsWith("schema")) .map(s ->
-		 * s.getKey()).collect(Collectors.toList());
-		 */
+		ObjectListing listing = client.listObjects(S3Client.getRootBucketName(), PERSONAL_EVENTS_S3_FOLDER);
 
 		List<String> dataKeys = listing.getObjectSummaries().stream()
 				.filter(s -> s.getKey().endsWith("json")).map(s -> s.getKey())
 				.collect(Collectors.toList());
 
-		/*
-		 * JSONObject schema;
-		 * 
-		 * for (String key : schemaKeys) { schema = new JSONObject(new
-		 * String(IOUtils.toByteArray(client.getObject(bucket,
-		 * key).getObjectContent()))); }
-		 */
-
-		ObjectMapper mapper = new ObjectMapper();
 		List<PersonalEvent> personalEvents = new ArrayList<PersonalEvent>();
 
-		try {
-			for (String key : dataKeys) {
-				byte[] jsonBytes = IOUtils.toByteArray(client.getObject(bucketName, key).getObjectContent());
-				PersonalEvent event = mapper.readValue(jsonBytes, PersonalEvent.class);
+		for (String key : dataKeys) {
+			S3Object obj = null;
+			try {
+				obj = client.getObject(S3Client.getRootBucketName(), key);
+			} catch (AmazonServiceException e) {
+				LOGGER.error(String.format("Error with S3 Service while trying to retrieve data for '%s'", key), e);
+			} catch (AmazonClientException e) {
+				LOGGER.error(String.format("Error with S3 Client while trying to retrieve data for '%s'", key), e);
+			} finally {
+				if (obj == null) {
+					continue;
+				}
+			}
+			PersonalEvent event = convertS3ObjectToPersonalEvent(obj);
+			if (event != null) {
 				personalEvents.add(event);
 			}
-		} catch (AmazonServiceException ase) {
-			LOGGER.error("Error with Amazon S3.", ase);
-		} catch (AmazonClientException ace) {
-			LOGGER.error("Error with Amazon S3 Client.", ace);
-		} catch (IOException ioe) {
-			LOGGER.error("Unexpected error (probably with converting data from S3 to JSON", ioe);
 		}
 
+		// Sort by descending date.
 		Collections.sort(personalEvents, (p1, p2) -> p2.getDate().compareTo(p1.getDate()));
-		
+
 		return personalEvents;
 	}
 
+	/**
+	 * Convert an S3Object with proper json formatting into a PersonalEvent object.
+	 * @param obj S3Object with proper json formatting for a PersonalEvent object
+	 * @return PersonalEvent object, or null if there was an exception, or null input.
+	 */
+	private PersonalEvent convertS3ObjectToPersonalEvent(S3Object obj) {
+		PersonalEvent event = null;
+		try {
+			byte[] jsonBytes = IOUtils.toByteArray(obj.getObjectContent());
+			event = MAPPER.readValue(jsonBytes, PersonalEvent.class);
+		} catch (IOException e) {
+			LOGGER.error("Error converting S3 object to PersonalEvent object.", e);
+		}
+
+		return event;
+	}
 }
