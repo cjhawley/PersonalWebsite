af9c5ca3828031f68b05bb172f662652411431cf
Chris Hawley <chris@goradar.com>
2019-12-25 09:49:34 -0600

    some cleanup of unused code, adding some comments

--- src/main/java/com/cjhawley/personal/ServiceRunner.java
+++ src/main/java/com/cjhawley/personal/ServiceRunner.java
@@ -1,10 +1,12 @@
 package com.cjhawley.personal;
 
-/**
- * Created by cjhawley on 12/23/16.
- */
 public class ServiceRunner {
 
+    /**
+     * Main entry point into running the service
+     * @param args arguments passed into the execution of this program
+     * @throws Exception Throws on unsuccessful (and unexpected) init and start of the program
+     */
     public static void main(final String[] args) throws Exception {
         PersonalWebsiteService personalWebsiteService = new PersonalWebsiteService(args);
         personalWebsiteService.init();
