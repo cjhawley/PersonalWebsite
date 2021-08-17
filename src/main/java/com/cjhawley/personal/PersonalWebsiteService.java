0fd2432517a60f4fc0a0a2f171b8957b077efa5f
Chris Hawley <chris@chrishawley.io>
2021-08-16 22:19:07 -0400

    Use freemarker over velocity engine (#94)
    * this commit uses freetype over velocity engine
    
    * fixing freetype loader

--- src/main/java/com/cjhawley/personal/PersonalWebsiteService.java
+++ src/main/java/com/cjhawley/personal/PersonalWebsiteService.java
@@ -1,63 +1,63 @@
 package com.cjhawley.personal;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.staticFiles;
+
+
 import com.cjhawley.personal.persistence.DiskPersonalEvents;
 import com.cjhawley.personal.persistence.PersonalEventsLoader;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import freemarker.template.Configuration;
 import io.norberg.automatter.jackson.AutoMatterModule;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
-
-
-import org.apache.velocity.tools.generic.DateTool;
 import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
-
-import com.google.gson.Gson;
-
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.staticFiles;
+import spark.template.freemarker.FreeMarkerEngine;
 
 /**
  * Implementation of the service
  */
 public class PersonalWebsiteService {
-	private static final String PERSONAL_EVENTS = "personal_events";
-	private static final ObjectMapper MAPPER = new ObjectMapper()
-			.registerModule(new AutoMatterModule());
+  private static final String PERSONAL_EVENTS = "personal_events";
+  private static final ObjectMapper MAPPER = new ObjectMapper()
+      .registerModule(new AutoMatterModule());
 
-	private final PersonalEventsLoader personalEvents;
+  private final PersonalEventsLoader personalEvents;
 
-	/**
-	 * Constructor
-	 * @param args We expect a single argument: the path to the personal events JSON file.
-	 * @throws Exception Throw an exception if we cannot load and parse the personal events file
-	 */
-	public PersonalWebsiteService(String[] args) throws Exception  {
-		File personalEventsFile = new File(args[0]);
+  /**
+   * Constructor
+   *
+   * @param args We expect a single argument: the path to the personal events JSON file.
+   * @throws Exception Throw an exception if we cannot load and parse the personal events file
+   */
+  public PersonalWebsiteService(String[] args) throws Exception {
+    final File personalEventsFile = new File(args[0]);
+    this.personalEvents = new DiskPersonalEvents(MAPPER, personalEventsFile);
+  }
 
-		this.personalEvents = new DiskPersonalEvents(MAPPER, personalEventsFile);
-	}
+  public void init() {
 
-	public void init() {
-		port(8080);
-		staticFiles.location("/web");
+    final Configuration freemarkerConfiguration = new Configuration(Configuration.getVersion());
+    freemarkerConfiguration.setClassForTemplateLoading(this.getClass(), "/web/jsp");
 
-		get("/ping", ((request, response) -> "pong"));
-		get("/", (request, response) -> {
-			Map<String, Object> model = new HashMap<>();
-			model.put(PERSONAL_EVENTS, personalEvents.getPersonalEvents());
-			model.put("date", new DateTool());
+    port(8080);
+    staticFiles.location("/web");
 
-			// The wm files are located under the resources directory
-			return new ModelAndView(model, "/web/jsp/index.vm");
-		}, new VelocityTemplateEngine());
+    get("/ping", ((request, response) -> "pong"));
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      model.put(PERSONAL_EVENTS, personalEvents.getPersonalEvents());
+      // The wm files are located under the resources directory
+      return new ModelAndView(model, "index.ftl");
+    }, new FreeMarkerEngine(freemarkerConfiguration));
 
-		final Gson gson = new Gson();
-		get("/personalevents", (request, response) -> {
-			response.header("content-type", "application/json");
-			return personalEvents.getPersonalEvents();
-		}, gson::toJson);
-	}
+    final Gson gson = new Gson();
+    get("/personalevents", (request, response) -> {
+      response.header("content-type", "application/json");
+      return personalEvents.getPersonalEvents();
+    }, gson::toJson);
+  }
 }
